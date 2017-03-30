e6b293b04ff04da855e3a1ea84a4dfd726f4eb81
aliaspider <aliaspider@gmail.com>
2017-03-30 20:22:58 +0100

    Jit64: remove every call to PtrOffset with a nullptr as base by using the value stored in RPPCSTATE instead.

--- Source/Core/Core/PowerPC/Jit64Common/Jit64PowerPCState.h
+++ Source/Core/Core/PowerPC/Jit64Common/Jit64PowerPCState.h
@@ -6,6 +6,7 @@
 
 #include "Common/CommonTypes.h"
 #include "Core/PowerPC/PowerPC.h"
+#include "Core/PowerPC/Jit64Common/Jit64Base.h"
 
 // We offset by 0x80 because the range of one byte memory offsets is
 // -0x80..0x7f.
@@ -17,3 +18,15 @@
 #define PPCSTATE_CTR PPCSTATE(spr[SPR_CTR])
 #define PPCSTATE_SRR0 PPCSTATE(spr[SPR_SRR0])
 #define PPCSTATE_SRR1 PPCSTATE(spr[SPR_SRR1])
+
+namespace Gen
+{
+inline OpArg MScaledPIC(X64Reg scaled, int scale, const void* ptr)
+{
+   return MComplex(RPPCSTATE, scaled, scale, PtrOffset(ptr, (char*)&PowerPC::ppcState + 0x80));
+}
+inline OpArg MDispPIC(X64Reg value, const void* ptr)
+{
+  return MScaledPIC(value, SCALE_1, ptr);
+}
+}
