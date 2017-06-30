8eb532405223ed68088c49d4a689aa93fb73592d
twinaphex <libretro@gmail.com>
2017-06-30 20:58:36 +0200

    Savestate save/load works now

--- Source/Core/DolphinLibretro/state.cpp
+++ Source/Core/DolphinLibretro/state.cpp
@@ -1,5 +1,6 @@
 #include <libretro.h>
 #include "Common/ChunkFile.h"
+#include "Core/Core.h"
 #include "Core/State.h"
 
 size_t retro_serialize_size(void)
@@ -14,15 +15,26 @@ size_t retro_serialize_size(void)
 
 bool retro_serialize(void* data, size_t size)
 {
-  PointerWrap p((u8**)&data, PointerWrap::MODE_WRITE);
-  State::DoState(p);
+   bool wasUnpaused = Core::PauseAndLock(true);
+   if (wasUnpaused)
+   {
+      PointerWrap p((u8**)&data, PointerWrap::MODE_WRITE);
+      State::DoState(p);
+   }
+   Core::PauseAndLock(false, wasUnpaused);
 
-  return true;
+   return true;
 }
+
 bool retro_unserialize(const void* data, size_t size)
 {
-  PointerWrap p((u8**)&data, PointerWrap::MODE_READ);
-  State::DoState(p);
+   bool wasUnpaused = Core::PauseAndLock(true);
+   if (wasUnpaused)
+   {
+      PointerWrap p((u8**)&data, PointerWrap::MODE_READ);
+      State::DoState(p);
+   }
+   Core::PauseAndLock(false, wasUnpaused);
 
-  return true;
+   return true;
 }
