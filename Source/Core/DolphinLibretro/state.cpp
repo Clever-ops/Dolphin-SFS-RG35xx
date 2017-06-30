794317f32467cb391f810772148089fa50e21242
twinaphex <libretro@gmail.com>
2017-06-30 21:19:54 +0200

    Buildfix

--- Source/Core/DolphinLibretro/state.cpp
+++ Source/Core/DolphinLibretro/state.cpp
@@ -6,30 +6,30 @@
 size_t retro_serialize_size(void)
 {
    size_t size = 0;
-   Core::PauseAndLock(true);
+   bool ret = Core::PauseAndLock(true);
    PointerWrap p((u8**)&size, PointerWrap::MODE_MEASURE);
    State::DoState(p);
-   Core::PauseAndLock(false, wasUnpaused);
+   Core::PauseAndLock(false, ret);
 
    return size;
 }
 
 bool retro_serialize(void* data, size_t size)
 {
-   Core::PauseAndLock(true);
+   bool ret = Core::PauseAndLock(true);
    PointerWrap p((u8**)&data, PointerWrap::MODE_WRITE);
    State::DoState(p);
-   Core::PauseAndLock(false, wasUnpaused);
+   Core::PauseAndLock(false, ret);
 
    return true;
 }
 
 bool retro_unserialize(const void* data, size_t size)
 {
-   Core::PauseAndLock(true);
+   bool ret = Core::PauseAndLock(true);
    PointerWrap p((u8**)&data, PointerWrap::MODE_READ);
    State::DoState(p);
-   Core::PauseAndLock(false, wasUnpaused);
+   Core::PauseAndLock(false, ret);
 
    return true;
 }
