8d88bc62fb7b65d8636c35001ce6c11e91653510
RealJohnGalt <johngalt@fake.mail>
2018-08-18 07:09:17 -0700

    Libretro: add disable efb to vram option

--- Source/Core/DolphinLibretro/Options.h
+++ Source/Core/DolphinLibretro/Options.h
@@ -77,6 +77,7 @@ extern Option<ShaderCompilationMode> shaderCompilationMode;
 extern Option<int> maxAnisotropy;
 extern Option<bool> efbScaledCopy;
 extern Option<bool> efbToTexture;
+extern Option<bool> efbToVram;
 extern Option<bool> gpuTextureDecoding;
 extern Option<bool> waitForShaders;
 }  // namespace Options
