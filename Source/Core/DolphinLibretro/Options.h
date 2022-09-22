7b8db4b99848fe88ab7db0b2c9811fc2d68e9c91
Nejsplitter <85600282+Nejsplitter@users.noreply.github.com>
2022-09-22 19:15:59 +0100

    Update Options.h

--- Source/Core/DolphinLibretro/Options.h
+++ Source/Core/DolphinLibretro/Options.h
@@ -99,6 +99,7 @@ extern Option<bool> progressiveScan;
 extern Option<bool> pal60;
 extern Option<int> maxAnisotropy;
 extern Option<bool> skipDupeFrames;
+extern Option<bool> immediatexfb;
 extern Option<bool> efbScaledCopy;
 extern Option<bool> forceTextureFiltering;
 extern Option<bool> efbToTexture;
